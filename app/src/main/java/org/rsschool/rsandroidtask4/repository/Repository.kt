c87b983b21546c1cafa766eef4ca69465887a1ac
avm82 <1982alexvm@gmail.com>
2021-09-10 21:51:09 +0300

    add Repository.kt

--- app/src/main/java/org/rsschool/rsandroidtask4/repository/Repository.kt
+++ app/src/main/java/org/rsschool/rsandroidtask4/repository/Repository.kt
@@ -1,4 +1,15 @@
 package org.rsschool.rsandroidtask4.repository
 
-class Repository {
+import kotlinx.coroutines.flow.Flow
+import org.rsschool.rsandroidtask4.data.Animal
+
+class Repository(private val db: AnimalsDataBase) {
+
+    private val dao get() = db.animalsDAO
+
+    fun getAll(): Flow<List<Animal>> = dao.getAll()
+
+    suspend fun save(animal: Animal) = dao.add(animal)
+
+    suspend fun delete(animal: Animal) = dao.delete(animal)
 }
