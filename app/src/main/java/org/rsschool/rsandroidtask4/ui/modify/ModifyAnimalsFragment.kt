855d89077c0a423178e4af159fe3427e74fd4685
avm82 <1982alexvm@gmail.com>
2021-09-16 00:50:45 +0300

    feature: validate modify form

--- app/src/main/java/org/rsschool/rsandroidtask4/ui/modify/ModifyAnimalsFragment.kt
+++ app/src/main/java/org/rsschool/rsandroidtask4/ui/modify/ModifyAnimalsFragment.kt
@@ -1,14 +1,18 @@
 package org.rsschool.rsandroidtask4.ui.modify
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import com.google.android.material.textfield.TextInputLayout
 import com.google.gson.GsonBuilder
 import dagger.hilt.android.AndroidEntryPoint
+import org.rsschool.rsandroidtask4.R
 import org.rsschool.rsandroidtask4.data.Animal
 import org.rsschool.rsandroidtask4.databinding.ModifyAnimalFragmentBinding
 import org.rsschool.rsandroidtask4.ui.main.MainViewModel
@@ -46,10 +50,35 @@ class ModifyAnimalsFragment : Fragment() {
                 age.editText?.setText(animal?.age.toString())
                 breed.editText?.setText(animal?.breed)
             }
+
+            inputName.addTextChangedListener(textWatcher(name))
+            inputAge.addTextChangedListener(textWatcher(age))
+
             toolbarTitle.text = arguments?.getString(ARG_TITLE)
             modifyButton.text = arguments?.getString(ARG_BUTTON_CAPTION)
             toolbar.setNavigationOnClickListener { goBack() }
-            modifyButton.setOnClickListener {
+            modifyButton.setOnClickListener { onClickModify() }
+        }
+    }
+
+    private fun textWatcher(view: TextInputLayout) = object : TextWatcher {
+        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+        override fun afterTextChanged(s: Editable?) {
+            view.error = ""
+        }
+    }
+
+
+    private fun ModifyAnimalFragmentBinding.onClickModify() {
+        when {
+            inputName.text.isNullOrBlank() -> {
+                name.error = getString(R.string.empty_error_text)
+            }
+            inputAge.text.isNullOrBlank() -> {
+                age.error = getString(R.string.empty_error_text)
+            }
+            else -> {
                 modify(makeAnimal(animal))
             }
         }
@@ -101,5 +130,5 @@ class ModifyAnimalsFragment : Fragment() {
             }
     }
 
-    private fun <T> views(block: ModifyAnimalFragmentBinding.() -> T): T? = binding.block()
+    private fun <T> views(block: ModifyAnimalFragmentBinding.() -> T): T = binding.block()
 }
